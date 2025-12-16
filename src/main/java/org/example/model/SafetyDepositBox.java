09601b808812bb0d45b71b5957a389661ba5afd2
Harsheel Singh <hsin212@aucklanduni.ac.nz>
2025-12-16 14:04:02 +1300

    git commit -m "Configured logs"

--- src/main/java/org/example/model/SafetyDepositBox.java
+++ src/main/java/org/example/model/SafetyDepositBox.java
@@ -1,6 +1,11 @@
 package org.example.model;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public abstract class SafetyDepositBox {
+
+    private static final Logger logger = LogManager.getLogger(SafetyDepositBox.class);
 
     private double id;
     private boolean isAllotted;
@@ -8,6 +13,7 @@ public abstract class SafetyDepositBox {
     public SafetyDepositBox(double id) {
         this.id = id;
         this.isAllotted = false;
+        logger.debug("Created SafetyDepositBox with ID: {}", id);
     }
 
     public boolean isAllotted() {
@@ -15,7 +21,15 @@ public abstract class SafetyDepositBox {
     }
 
     public void setAllotted(boolean allotted) {
+        logger.debug("Box ID {} allotment status changed from {} to {}",
+                id, this.isAllotted, allotted);
         this.isAllotted = allotted;
+
+        if (allotted) {
+            logger.info("Box ID {} has been allocated", id);
+        } else {
+            logger.info("Box ID {} has been released", id);
+        }
     }
 
     public double getId() {
@@ -23,6 +37,7 @@ public abstract class SafetyDepositBox {
     }
 
     public void setId(double id) {
+        logger.debug("Box ID changed from {} to {}", this.id, id);
         this.id = id;
     }
 }
